54b1a1cc4813d9750f254e16bb236da96d1265ee
ebimo21 <ebi4jah15@gmail.com>
2022-11-09 23:31:53 +0100

    fix:boxicon

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { footer, social } from '../data/content-text'
 
-// import 'boxicons'
+import 'boxicons'
 import Link from 'next/link'
 
 export const Footer = () => {
